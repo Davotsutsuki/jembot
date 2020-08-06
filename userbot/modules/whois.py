dd90ab2f03f7ac5463c7eb5c67fa4a97b6652ae2
Davotsutsuki <67810035+Davotsutsuki@users.noreply.github.com>
2020-08-06 16:57:54 +0700

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -17,7 +17,7 @@ from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
 
 
-@register(pattern=".whois(?: |$)(.*)", outgoing=True)
+@register(pattern=".siapa(?: |$)(.*)", outgoing=True)
 async def who(event):
 
     await event.edit(
@@ -134,21 +134,12 @@ async def fetch_info(replied_user, event):
     caption += f"Last Name: {last_name}\n"
     caption += f"Username: {username}\n"
     caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
-    caption += f"ID: <code>{user_id}</code>\n\n"
-    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += "Permanent Link To Profile: "
-    caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
     return photo, caption
 
 
 CMD_HELP.update({
     "whois":
-    "`.whois` <username> or reply to someones text with .whois"
+    "`.siapa` <username> or reply to someones text with .whois"
     "\nUsage: Gets info of an user."
 })
